74ccc0e836add4b125342c2e8e1df5792ca2d83d
kapperson745 <pirateezra@gmail.com>
2024-12-01 03:24:45 -0500

    Add files via upload

---
+++ Module 6 Assignment - Fundamental concepts of Python - IDLE.py
@@ -0,0 +1,8 @@
+# this file will print "It is raining cats and dogs.".
+#Set variable c to "cats." Set variable d to "dogs." Set variable s to the sentence to print
+c = "cats"
+d = "dogs."
+s = "It is raining cats and dogs."
+print (s)
+
+# for the record you never said i had to use the c and d variables inside the s variable, the assignment just says they have to exist
